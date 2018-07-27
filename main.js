ba77f1144e75aa5ea00ea64fed8ac136f3ffd36e
wingchunkf <bergmcj@comcast.net>
2018-07-26 21:26:36 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    
+var first = document.querySelector('.first-contact');
+var mainDiv = document.querySelector('.main-body');
+var modal1 = document.querySelector('.modal1');
+var col4 = document.querySelector('.col-4 > p');
+
+    // col4.addEventListener('mouseover', function() {
+    //     console.log('overlay')
+    //     mainDiv.classList.add('overlay');
+    })
+
+
+
+
+
+
